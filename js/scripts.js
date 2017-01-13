7af16ba4d8b765f5c35ef45fcc7174404c8daf88
Kory Andrew Skarbek <Skarski@Korys-MacBook-Pro.local>
2017-01-13 10:28:51 -0800

    add text to 4 select element. add the start to the if statement to determind the users total add value to option element for 4th select element.

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,31 @@
 
 
 // User interface (or front-end) logic:
-$(document).ready(function(){
-  $("formSubmit").submit(function(event){
+$(document).ready(function() {
+  $("#formSubmit").click(function(event) {
     event.preventDefault();
+    $(".hide-me").hide();
     var userChoice1 = parseInt($("#question1").val());
-    var userChoice2 = ParseInt($("#question2").val());
+    var userChoice2 = parseInt($("#question2").val());
     var userChoice3 = parseInt($("#question3").val());
     var userChoice4 = parseInt($("#question4").val());
     var userChoice5 = parseInt($("#question5").val());
-    var userTotal = (userChoice1 + userChoice2 + userChoice3 + userChoice4 + userChoice5);
+    var userTotal = userChoice1 + userChoice2 + userChoice3 + userChoice4 + userChoice5;
 
     console.log(userTotal)
-    if ()
+    if (userTotal >= 500) {
+      $("#cssUnhide").show();
+    } else if (userTotal >=  && <=) {
+      $("#cUnhide").show();
+    } else if (userTotal >= ) {
+      $("#javaUnhide").show();
+    } else if (userTotal >= ) {
+      $("#phpUnhide").show();
+    } else if (userTotal >= ) {
+      $("#rubyUnhide").show();
+    } else {
+
+    }
+
   });
 });
