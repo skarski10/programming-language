ed98720a8a0f29aa6ed3c60e1dc34159fd735eaf
Kory Andrew Skarbek <Skarski@Korys-MacBook-Pro.local>
2017-01-13 09:23:13 -0800

    add submit function for button in javascript with event and event.preventDefault. add 5 variables the take the selections of the user for each question as their value.

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,12 @@
 
 // User interface (or front-end) logic:
 $(document).ready(function(){
-  
+  $("formSubmit").submit(function(event){
+    event.preventDefault();
+    var userChoice1 = $("#question1").val();
+    var userChoice2 = $("#question2").val();
+    var userChoice3 = $("#question3").val();
+    var userChoice4 = $("#question4").val();
+    var userChoice5 = $("#question5").val();
+  })
 });
