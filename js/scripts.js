296e4d9dcb22810cb78958ff345d8edeedaa1d75
Kory Andrew Skarbek <Skarski@Korys-MacBook-Pro.local>
2017-01-13 09:34:37 -0800

    add text for 3rd select form. Add values for the the option elements in the first 3 select elements.

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,14 @@
 $(document).ready(function(){
   $("formSubmit").submit(function(event){
     event.preventDefault();
-    var userChoice1 = $("#question1").val();
-    var userChoice2 = $("#question2").val();
-    var userChoice3 = $("#question3").val();
-    var userChoice4 = $("#question4").val();
-    var userChoice5 = $("#question5").val();
-  })
+    var userChoice1 = parseInt($("#question1").val());
+    var userChoice2 = ParseInt($("#question2").val());
+    var userChoice3 = parseInt($("#question3").val());
+    var userChoice4 = parseInt($("#question4").val());
+    var userChoice5 = parseInt($("#question5").val());
+    var userTotal = (userChoice1 + userChoice2 + userChoice3 + userChoice4 + userChoice5);
+
+    console.log(userTotal)
+    if ()
+  });
 });
